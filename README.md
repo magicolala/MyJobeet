63a560bde04d9d27ae36547660acf28f6aa627f7
Cédric Oloa <cedric.oloa@laposte.net>
2016-09-26 23:16:04 +0200

    First Commit save

---
+++ README.md
@@ -0,0 +1,4 @@
+MyJobeet
+========
+
+A Symfony project created on September 25, 2016, 10:08 pm.
---
+++ src/Ens/JobeetBundle/Entity/Category.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Ens\JobeetBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Category
+ *
+ * @ORM\Table(name="category")
+ * @ORM\Entity(repositoryClass="Ens\JobeetBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+    * @ORM\OneToMany(targetEntity="Job", mappedBy="category")
+    *
+    */
+    private $jobs;
+
+
+    /**
+    * @ORM\OneToMany(targetEntity="CategoryAffiliate", mappedBy="category")
+    */
+    private $category_affiliates;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function __construct()
+    {
+        $this->jobs = new ArrayCollection();
+        $this->category_affiliates = new ArrayCollection();
+    }
+
+    /**
+     * Add jobs
+     *
+     * @param \Ens\JobeetBundle\Entity\Job $jobs
+     * @return Category
+     */
+    public function addJob(\Ens\JobeetBundle\Entity\Job $jobs)
+    {
+        $this->jobs[] = $jobs;
+
+        return $this;
+    }
+
+    /**
+     * Remove jobs
+     *
+     * @param \Ens\JobeetBundle\Entity\Job $jobs
+     */
+    public function removeJob(\Ens\JobeetBundle\Entity\Job $jobs)
+    {
+        $this->jobs->removeElement($jobs);
+    }
+
+    /**
+     * Get jobs
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getJobs()
+    {
+        return $this->jobs;
+    }
+
+    /**
+     * Add category_affiliates
+     *
+     * @param \Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates
+     * @return Category
+     */
+    public function addCategoryAffiliate(\Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates)
+    {
+        $this->category_affiliates[] = $categoryAffiliates;
+
+        return $this;
+    }
+
+    /**
+     * Remove category_affiliates
+     *
+     * @param \Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates
+     */
+    public function removeCategoryAffiliate(\Ens\JobeetBundle\Entity\CategoryAffiliate $categoryAffiliates)
+    {
+        $this->category_affiliates->removeElement($categoryAffiliates);
+    }
+
+    /**
+     * Get category_affiliates
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getCategoryAffiliates()
+    {
+        return $this->category_affiliates;
+    }
+
+    public function __toString()
+    {
+      return $this->getName();
+    }
+}
